7463014179ac5929dbdcfcc04c01fdb0ea64248b
caocaoer <caocaoer1019@163.com>
2016-08-15 19:36:33 +0800

    add

---
+++ json.js
@@ -0,0 +1,50 @@
+var people = {
+        "firstName": "Brett",
+        "lastName": "McLaughlin",
+        "email": "aaaa"
+};
+
+people.email
+
+var peoples = ["Brett", "McLaughlin", "aaaa", "aaaa", "aaaa", "aaaa", "aaaa"];
+people[1]
+
+
+
+var people = {
+    "programmers": [{
+        "firstName": "Brett",
+        "lastName": "McLaughlin",
+        "email": "aaaa"
+    }, {
+        "firstName": "Jason",
+        "lastName": "Hunter",
+        "email": "bbbb"
+    }, {
+        "firstName": "Elliotte",
+        "lastName": "Harold",
+        "email": "cccc"
+    }],
+    "authors": [{
+        "firstName": "Isaac",
+        "lastName": "Asimov",
+        "genre": "sciencefiction"
+    }, {
+        "firstName": "Tad",
+        "lastName": "Williams",
+        "genre": "fantasy"
+    }, {
+        "firstName": "Frank",
+        "lastName": "Peretti",
+        "genre": "christianfiction"
+    }],
+    "musicians": [{
+        "firstName": "Eric",
+        "lastName": "Clapton",
+        "instrument": "guitar"
+    }, {
+        "firstName": "Sergei",
+        "lastName": "Rachmaninoff",
+        "instrument": "piano"
+    }]
+};
